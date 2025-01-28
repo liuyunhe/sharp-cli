d4dac75d6cd7a046bc8586893462e5bad11cc601
liuyunhe <liuyunhe666@aliyun.com>
2025-01-29 00:24:53 +0800

    test(cli): 添加 CLI 命令测试用例
    - 新增测试用例以验证 CLI 命令的行为
    - 测试了错误命令、帮助命令、版本命令和调试命令的执行情况
    - 使用 execa 执行 CLI 命令并捕获输出
    - 优化了测试代码结构，提高了可读性和可维护性

--- imooc-cli/packages/cli/__tests__/cli.test.js
+++ imooc-cli/packages/cli/__tests__/cli.test.js
@@ -1,7 +1,47 @@
-'use strict';
+import path from 'node:path'
+import { execa } from 'execa'
+import { console } from 'node:inspector'
 
-const cli = require('..');
-const assert = require('assert').strict;
+const cli = path.resolve(__dirname, '../bin/cli.js')
 
-assert.strictEqual(cli(), 'Hello from cli');
-console.info('cli tests passed');
+const bin =
+  () =>
+  (...args) =>
+    execa(cli, args)
+
+// 测试运行错误的命令
+test('run error command', async () => {
+  const { stderr } = await bin()('iii')
+  expect(stderr).toContain('未知的命令:iii')
+})
+
+// 测试运行帮助命令
+test('should not throw error when run help', async () => {
+  let error = null
+  try {
+    await bin()('-h')
+  } catch (e) {
+    error = e
+  }
+  expect(error).toBeNull()
+})
+
+// 测试运行版本命令
+test('show correct version when run --version or -V', async () => {
+  const { stdout: version1 } = await bin()('--version')
+  const { stdout: version2 } = await bin()('-V')
+  expect(version1).toContain(require('../package.json').version)
+  expect(version1).toBe(version2)
+})
+
+// 测试运行debug命令
+
+test('open debug mode', async () => {
+  let error = null
+  try {
+    await bin()('--debug')
+  } catch (e) {
+    error = e
+  }
+  expect(error.message).toContain('开启调试模式')
+})
