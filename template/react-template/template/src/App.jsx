63031489316a37f903c2071e98f3267bc80ab1e2
liuyunhe <liuyunhe666@aliyun.com>
2025-02-04 14:37:17 +0800

    feat(template): 添加 React 和 Vue 项目模板
    - 新增 React 和 Vue 项目模板文件夹
    - 添加相应的 package.json、README.md、vite.config.js 等配置文件
    - 创建项目结构，包括 src、public 等目录
    - 配置 ESLint、依赖库和开发脚本

---
+++ template/react-template/template/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello react-template</h1>
+    </>
+  )
+}
+
+export default App
---
+++ template/vue-template/template/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <h1>
+    hello vue-template
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
