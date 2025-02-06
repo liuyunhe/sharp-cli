c34ab60d45e8826551c9e8ca763656ef4b3542e4
liuyunhe <liuyunhe666@aliyun.com>
2025-02-06 16:00:16 +0800

    feat(init): 添加模板引擎功能
    - 引入 ejs 和 glob 依赖
    - 实现 ejs 渲染功能，用于定制化模板
    - 更新 installTemplate 函数，增加 ejs 渲染步骤

--- imooc-cli/packages/init/lib/installTemplate.js
+++ imooc-cli/packages/init/lib/installTemplate.js
@@ -2,11 +2,13 @@ import path from 'node:path'
 import fse from 'fs-extra'
 import ora from 'ora'
 import { pathExistsSync } from 'path-exists'
+import ejs from 'ejs'
+import { glob } from 'glob'
 import { log } from '@sharpcli/utils'
 
 /**
  * 根据目标路径和模板对象生成缓存文件路径
- * 
+ *
  * @param {string} targetPath - 目标路径，用于定位缓存文件的位置
  * @param {Object} template - 模板对象，包含模板的npm名称等信息
  * @returns {string} 返回缓存文件的绝对路径
@@ -19,10 +21,10 @@ function getCacheFilePath(targetPath, template) {
 
 /**
  * 复制文件到指定目录
- * 
+ *
  * 本函数用于将缓存文件夹中的模板文件复制到指定的安装目录中
  * 主要用于初始化项目或配置环境时，将预定义的模板文件复制到用户指定的位置
- * 
+ *
  * @param {string} targetPath - 目标路径，用于确定缓存文件夹的位置
  * @param {object} template - 模板对象，包含模板的npm名称等信息，用于确定缓存文件夹的位置
  * @param {string} installDir - 安装目录，即目标目录，文件将被复制到此目录下
@@ -42,6 +44,48 @@ function copyFile(targetPath, template, installDir) {
   })
   // 加载指示器显示成功信息：模板文件复制完成
   spinner.succeed('模版拷贝完成')
+}
+
+/**
+ * 使用EJS模板引擎渲染文件
+ * 
+ * @param {string} installDir - 文件搜索的根目录
+ * @param {Object} template - 模板对象，包含忽略项和值
+ * @param {string} name - 传递给模板的数据名称
+ */
+function ejsRender(installDir, template, name) {
+  // 解构模板对象，获取忽略项数组和值，如果未提供则使用默认空数组
+  const { ignore = [] } = template
+  // 创建包含名称数据的对象，用于EJS模板渲染
+  const ejsData = {
+    data: {
+      name
+    }
+  }
+  // 使用glob函数异步获取安装目录下所有文件路径，忽略node_modules目录和其他指定的忽略项
+  glob('**', {
+    cwd: installDir,
+    nodir: true,
+    ignore: [...ignore, '**/node_modules/**']
+  }).then((files) => {
+    // 遍历每个文件路径
+    files.map((file) => {
+      // 解析文件的绝对路径
+      const filePath = path.resolve(installDir, file)
+      // 记录文件路径的日志信息
+      log.verbose('ejsRenderFilePath', filePath)
+      // 使用EJS模板引擎渲染文件，传入数据和回调函数处理错误或写入结果
+      ejs.renderFile(filePath, ejsData, (err, result) => {
+        if (err) {
+          // 如果发生错误，记录错误信息
+          log.error(err)
+        } else {
+          // 如果没有错误，将渲染结果写回文件
+          fse.writeFileSync(filePath, result)
+        }
+      })
+    })
+  })
 }
 
 /**
@@ -81,4 +125,6 @@ export default async function installTemplate(selectedTemplate, opts) {
   }
   // 复制模板文件到安装目录
   copyFile(targetPath, template, installDir)
+  // 使用ejs模板引擎渲染安装目录中的文件
+  ejsRender(installDir, template, name)
 }
