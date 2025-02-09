f90b2aee93bc3a35e64f831c9c3651d0c9bde91a
liuyunhe <liuyunhe666@aliyun.com>
2025-02-09 22:57:45 +0800

    feat(install): 搜索 Vue 仓库并打印结果
    - 在 init 后添加搜索 Repositories 逻辑
    - 搜索参数：语言为 Vue，按星数降序排序，获取前 5 条结果
    - 打印搜索结果的 items

--- imooc-cli/packages/install/lib/index.js
+++ imooc-cli/packages/install/lib/index.js
@@ -42,7 +42,14 @@ class InstallCommand extends Command {
     }
     gitAPI.savePlatform(platform)
     await gitAPI.init()
-    
+    const searchResult = await gitAPI.searchRepositories({
+      q: 'vue+language:vue',
+      order: 'desc',
+      sort: 'stars',
+      per_page: 5,
+      page: 1
+    })
+    console.log(searchResult.items)
   }
 }
 
