a998e17d299737c2f5468df88b1f9d9698b49d9f
liuyunhe <liuyunhe666@aliyun.com>
2025-02-10 15:55:30 +0800

    feat(install): 支持 GitHub 和 Gitee 的搜索和下载功能
    - 新增 Gitee 支持
    - 实现搜索模式选择（仓库搜索、代码搜索）
    - 添加搜索关键词和语言输入
    - 实现分页功能（上一页、下一页）
    - 优化搜索结果展示

--- imooc-cli/packages/install/lib/index.js
+++ imooc-cli/packages/install/lib/index.js
@@ -1,5 +1,17 @@
 import Command from '@sharpcli/command'
-import { log, makeList, Github, getGitPlatform } from '@sharpcli/utils'
+import {
+  log,
+  makeList,
+  Github,
+  Gitee,
+  getGitPlatform,
+  makeInput
+} from '@sharpcli/utils'
+
+const PREV_PAGE = '${prev_page}'
+const NEXT_PAGE = '${next_page}'
+const SEARCH_MODE_REPO = 'search_repo'
+const SEARCH_MODE_CODE = 'search_code'
 
 class InstallCommand extends Command {
   get command() {
@@ -18,8 +30,13 @@ class InstallCommand extends Command {
   }
 
   async action(params) {
+    await this.generateGitAPI()
+    await this.searchGitAPI()
+  }
+
+  async generateGitAPI() {
     let platform = getGitPlatform()
-    if (!platform) { 
+    if (!platform) {
       platform = await makeList({
         choices: [
           {
@@ -37,19 +54,139 @@ class InstallCommand extends Command {
     let gitAPI
     if (platform === 'github') {
       gitAPI = new Github()
-    } else { 
-
+    } else {
+      gitAPI = new Gitee()
     }
     gitAPI.savePlatform(platform)
     await gitAPI.init()
-    const searchResult = await gitAPI.searchRepositories({
-      q: 'vue+language:vue',
-      order: 'desc',
-      sort: 'stars',
-      per_page: 5,
-      page: 1
+    this.gitAPI = gitAPI
+  }
+
+  async searchGitAPI() {
+    const platform = this.gitAPI.getPlatform()
+
+    if (platform === 'github') {
+      this.mode = await makeList({
+        choices: [
+          {
+            name: '搜索仓库',
+            value: SEARCH_MODE_REPO
+          },
+          {
+            name: '搜索代码',
+            value: SEARCH_MODE_CODE
+          }
+        ],
+        message: '请选择搜索模式'
+      })
+    } else {
+      this.mode = SEARCH_MODE_REPO
+    }
+    // 1. 收集搜索关键词和开发语言
+    this.q = await makeInput({
+      message: '请输入搜索关键词',
+      validate(value) {
+        if (value.length > 0) {
+          return true
+        } else {
+          return '请输入搜索关键词'
+        }
+      }
     })
-    console.log(searchResult.items)
+    this.language = await makeInput({
+      message: '请输入开发语言'
+    })
+    log.verbose(
+      'search key words',
+      this.q,
+      this.language,
+      platform,
+    )
+    this.page = 1
+    this.perPage = 10
+    await this.doSearch()
+  }
+  async doSearch() {
+    const platform = this.gitAPI.getPlatform()
+
+    let count = 0
+    let list = []
+    let searchResult
+    // 2. 生成搜索参数
+    if (platform === 'github') {
+      const params = {
+        q: this.q + (this.language ? ` language:${this.language}` : ''),
+        order: 'desc',
+        sort: 'stars',
+        per_page: this.perPage,
+        page: this.page
+      }
+      log.verbose('search params', params)
+      if (this.mode === SEARCH_MODE_REPO) { 
+        searchResult = await this.gitAPI.searchRepositories(params)
+        list = searchResult.items.map((item) => ({
+          name: `${item.full_name}\n(${item.description})`,
+          value: item.full_name
+        }))
+      } else {
+        searchResult = await this.gitAPI.searchCode(params)
+        list = searchResult.items.map((item) => ({
+          name:
+            item.repository.full_name + '\n' +
+            (item.repository.description
+              ? `(${item.repository.description})`
+              : ''),
+          value: item.repository.full_name
+        }))
+      }
+      count = searchResult.total_count // 整体数据量
+      log.verbose('searchResult.items.length', searchResult.items.length)
+    }
+
+    // 判断当前页面，已经是否到达最大页数
+    if (
+      (platform === 'github' && this.page * this.perPage < count) ||
+      list.length > 0
+    ) {
+      list.push({
+        name: '下一页',
+        value: NEXT_PAGE
+      })
+    }
+    if (this.page > 1) {
+      list.unshift({
+        name: '上一页',
+        value: PREV_PAGE
+      })
+    }
+
+    const keyword = await makeList({
+      message:
+        platform === 'github'
+          ? `请选择要下载的项目（共${count}条数据）`
+          : '请选择要下载的项目',
+      choices: list
+    })
+
+    if (count > 0) {
+      if (keyword === NEXT_PAGE) {
+        await this.nextPage()
+      } else if (keyword === PREV_PAGE) {
+        await this.prevPage()
+      } else {
+        // 下载项目
+        this.keyword = keyword
+      }
+    }
+  }
+  async nextPage() {
+    this.page++
+    await this.doSearch()
+  }
+
+  async prevPage() {
+    this.page--
+    await this.doSearch()
   }
 }
 
