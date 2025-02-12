b48a793aa496d8a5132b0c1e213fd5feed7cd74e
liuyunhe <liuyunhe666@aliyun.com>
2025-02-12 10:57:38 +0800

    feat(lint): 添加 ESLint 对 Vue 项目的代码检查功能
    - 新增 vueConfig.js 文件，定义 ESLint 对 Vue 项目的配置
    - 在 index.js 中实现 ESLint 检查的完整流程，包括：
      - 安装依赖
      - 执行 ESLint 检查
      - 解析和输出 ESLint 结果
    -

--- imooc-cli/packages/lint/lib/index.js
+++ imooc-cli/packages/lint/lib/index.js
@@ -1,6 +1,9 @@
+import { ESLint } from 'eslint'
+import { execa } from 'execa'
+import ora from 'ora'
 import Command from '@sharpcli/command'
 import { log, printErrorLog, makeList } from '@sharpcli/utils'
-
+import vueConfig from './eslint/vueConfig.js'
 
 class LintCommand extends Command {
   get command() {
@@ -15,9 +18,68 @@ class LintCommand extends Command {
     return []
   }
 
+  extractESLint(resultText, type) {
+    const problems = /[0-9]+ problems/
+    const warnings = /([0-9]+) warnings/
+    const errors = /([0-9]+) errors/
+    switch (type) {
+      case 'problems':
+        return resultText.match(problems)[0].match(/[0-9]+/)[0]
+      case 'warnings':
+        return resultText.match(warnings)[0].match(/[0-9]+/)[0]
+      case 'errors':
+        return resultText.match(errors)[0].match(/[0-9]+/)[0]
+      default:
+        return null
+    }
+  }
+
+  parseESLintResult(resultText) {
+    const problems = this.extractESLint(resultText, 'problems')
+    const errors = this.extractESLint(resultText, 'errors')
+    const warnings = this.extractESLint(resultText, 'warnings')
+    return {
+      problems: +problems || 0,
+      errors: +errors || 0,
+      warnings: +warnings || 0
+    }
+  }
+
   async action() {
     log.verbose('lint')
-    
+    // 1. eslint
+    // 准备工作，安装依赖
+    const spinner = ora('正在安装依赖').start()
+    try {
+      await execa('npm', ['install', '-D', 'eslint-plugin-vue'])
+      await execa('npm', ['install', '-D', 'eslint-config-airbnb-base'])
+    } catch (e) {
+      printErrorLog(e)
+    } finally {
+      spinner.stop()
+    }
+    log.info('正在执行eslint检查')
+    // 执行工作，eslint
+    const cwd = process.cwd()
+    const eslint = new ESLint({
+      cwd,
+      overrideConfig: vueConfig
+    })
+    const results = await eslint.lintFiles(['src/**/*.js', 'src/**/*.vue'])
+    const formatter = await eslint.loadFormatter('stylish')
+    const resultText = formatter.format(results)
+    console.log(resultText)
+    const eslintResult = this.parseESLintResult(resultText)
+    log.verbose('eslintResult', eslintResult)
+    //   "devDependencies": {
+    //   "eslint-config-airbnb-base": "^15.0.0",
+    //   "eslint-plugin-vue": "^9.32.0"
+    // }
+    log.success(
+      'eslint检查完毕',
+      '错误: ' + eslintResult.errors,
+      '，警告: ' + eslintResult.warnings
+    )
   }
 }
 
