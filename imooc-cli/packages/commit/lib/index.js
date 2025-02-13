d12f630154c16724a6d949c653d47c03c492f7bf
liuyunhe <liuyunhe666@aliyun.com>
2025-02-13 17:26:10 +0800

    feat(commit): 增加代码自动化提交和发布功能
    - 新增 commit 和 publish 命令选项
    - 实现代码自动化提交流程，包括版本号生成、冲突检查、分支合并等
    - 实现代码发布流程，包括创建 tag、合并到 master 分支、删除分支等
    - 增加对远程分支和 tag 的管理
    - 优化缓存清理功能

--- imooc-cli/packages/commit/lib/index.js
+++ imooc-cli/packages/commit/lib/index.js
@@ -2,6 +2,7 @@ import path from 'node:path'
 import fs from 'node:fs'
 import fse from 'fs-extra'
 import SimpleGit from 'simple-git'
+import semver from 'semver'
 import Command from '@sharpcli/command'
 import {
   log,
@@ -26,10 +27,13 @@ class CommitCommand extends Command {
   }
 
   get options() {
-    return [['-c, --clear', '清空缓存', false]]
+    return [
+      ['-c, --clear', '清空缓存', false],
+      ['-p, --publish', '发布', false]
+    ]
   }
 
-  async action([{ clear }]) {
+  async action([{ publish, clear }]) {
     log.verbose('commit')
     log.verbose('commit params clear', clear)
     if (clear) {
@@ -38,6 +42,11 @@ class CommitCommand extends Command {
 
     await this.createRemoteRepo()
     await this.initLocal()
+    await this.commit()
+
+    if (publish) {
+      await this.publish()
+    }
   }
 
   // 阶段1：创建远程仓库
@@ -125,6 +134,217 @@ class CommitCommand extends Command {
       // 直接推送当前到master分支
       await this.pushRemoteRepo('master')
     }
+  }
+
+  // 阶段3：代码自动化提交
+  async commit() {
+    // 自动生成版本号
+    await this.getCorrectVersion()
+
+    await this.checkStash()
+    await this.checkConflicted()
+    await this.checkNotCommitted()
+    await this.checkoutBranch(this.branch)
+    await this.pullRemoteMasterAndBranch()
+    await this.pushRemoteRepo(this.branch)
+  }
+
+  async publish() {
+    await this.checkTag()
+    await this.checkoutBranch('master')
+    await this.mergeBranchToMaster()
+    await this.pushRemoteRepo('master')
+    await this.deleteLocalBranch()
+    await this.deleteRemoteBranch()
+  }
+
+  async deleteLocalBranch() {
+    log.info('删除本地分支')
+    await this.git.deleteLocalBranch(this.branch)
+    log.success('删除本地分支成功')
+  }
+
+  async deleteRemoteBranch() {
+    log.info('删除远程分支')
+    await this.git.push(['origin', '--delete', this.branch])
+    log.success('删除远程分支成功')
+  }
+
+  async mergeBranchToMaster() {
+    log.info(`[${this.branch}] -> [master]`)
+    await this.git.mergeFromTo(this.branch, 'master')
+    log.success(`合并成功 [${this.branch}] -> [master]`)
+  }
+
+  async checkTag() {
+    log.info('获取远程 tag 列表')
+    const tag = `release/${this.version}`
+    const tagList = await this.getRemoteBranchList('release')
+    log.verbose('tagList', tagList, this.version)
+    if (tagList.includes(this.version)) {
+      log.info(`远程${tag} 已存在`)
+      // 删除tag也是通过push命令
+      await this.git.push(['origin', `:refs/tags/${tag}`])
+      log.success(`删除远程 ${tag} 成功`)
+    } else {
+      log.success(`${tag} 不存在，可以发布`)
+    }
+    const localTagList = await this.git.tags()
+    if (localTagList.all.includes(tag)) {
+      log.info(`本地tag ${tag} 已存在`)
+      await this.git.tag(['-d', tag])
+      log.success('删除本地 tag 成功')
+    }
+    await this.git.addTag(tag)
+    log.success(`创建本地 tag ${tag} 成功`)
+    await this.git.pushTags('origin')
+    log.success(`推送远程 tag ${tag} 成功`)
+  }
+
+  async pullRemoteMasterAndBranch() {
+    log.info(`合并 [master] -> [${this.branch}]`)
+    await this.pullRemoteRepo('master')
+    log.success('合并远程 [master] 分支成功')
+    log.info('检查远程分支')
+    const remoteBranchList = await this.getRemoteBranchList()
+    if (remoteBranchList.indexOf(this.version) >= 0) {
+      log.info(`合并 [${this.branch}] -> [${this.branch}]`)
+      await this.pullRemoteRepo(this.branch)
+      log.success(`合并远程 [${this.branch}] 分支成功`)
+      await this.checkConflicted()
+    } else {
+      log.success('不存在远程分支')
+    }
+  }
+
+  // 自动切换分支
+  async checkoutBranch(branchName) {
+    const localBranchList = await this.git.branchLocal()
+    if (localBranchList.all.indexOf(branchName) >= 0) {
+      await this.git.checkout(branchName)
+    } else {
+      // 创建分支
+      await this.git.checkoutLocalBranch(branchName)
+    }
+
+    log.success(`本地分支切换到 ${branchName}`)
+  }
+
+  async checkConflicted() {
+    log.info('代码冲突检查')
+    const status = await this.git.status()
+    if (status.conflicted.length > 0) {
+      throw new Error('代码冲突，请手动处理后再提交')
+    }
+    log.success('代码无冲突')
+  }
+
+  // 缓冲区的代码检查
+  async checkStash() {
+    log.info('检查 stash 记录')
+    // 获取未提交的文件
+    const stashList = await this.git.stashList()
+
+    if (stashList && stashList.all.length > 0) {
+      await this.git.stash(['pop'])
+      log.success('stash pop 成功')
+    }
+  }
+
+  async getCorrectVersion() {
+    log.info('获取分支')
+    const remoteBranchList = await this.getRemoteBranchList('release')
+
+    let releaseVersion = ''
+    if (remoteBranchList && remoteBranchList.length > 0) {
+      releaseVersion = remoteBranchList[0]
+    }
+
+    const devVersion = this.version
+    log.verbose('devVersion', devVersion)
+    log.verbose('releaseVersion', releaseVersion)
+    if (!releaseVersion) {
+      this.branch = `dev/${devVersion}`
+    } else if (semver.gt(devVersion, releaseVersion)) {
+      log.info(`当前版本大于线上release版本，${devVersion} > ${releaseVersion}`)
+      this.branch = `dev/${devVersion}`
+    } else {
+      log.info(
+        `当前版本小于等于线上release版本，${devVersion} <= ${releaseVersion}`
+      )
+      const incType = await makeList({
+        message: '请选择版本号',
+        defaultValue: 'patch',
+        // x,y,z
+        // 'patch', 'minor', 'major'
+        choices: [
+          {
+            name: `小版本(${releaseVersion}) -> ${semver.inc(
+              releaseVersion,
+              'patch'
+            )}`,
+            value: 'patch'
+          },
+          {
+            name: `中版本(${releaseVersion}) -> ${semver.inc(
+              releaseVersion,
+              'minor'
+            )}`,
+            value: 'minor'
+          },
+          {
+            name: `大版本(${releaseVersion}) -> ${semver.inc(
+              releaseVersion,
+              'major'
+            )}`,
+            value: 'major'
+          }
+        ]
+      })
+      const incVersion = semver.inc(releaseVersion, incType)
+      this.branch = `dev/${incVersion}`
+      this.version = incVersion
+      this.syncVersionToPackageJson()
+    }
+    log.success(`分支获取成功 ${this.branch}`)
+  }
+
+  syncVersionToPackageJson() {
+    const dir = process.cwd()
+    const pkgPath = path.resolve(dir, 'package.json')
+    const pkg = fse.readJsonSync(pkgPath)
+    if (pkg && pkg.version !== this.version) {
+      pkg.version = this.version
+      fse.writeJsonSync(pkgPath, pkg, { spaces: 2 })
+    }
+  }
+
+  async getRemoteBranchList(type) {
+    const remoteList = await this.git.listRemote(['--refs'])
+    let reg
+    if (type === 'release') {
+      // release/0.0.1
+      reg = /.+?refs\/tags\/release\/(\d+\.\d+\.\d+)/
+    } else {
+      // dev/0.0.1
+      reg = /.+?refs\/tags\/dev\/(\d+\.\d+\.\d+)/
+    }
+    log.verbose('remoteList', remoteList)
+    return remoteList
+      .split('\n')
+      .map((remote) => {
+        const match = reg.exec(remote)
+        if (match && semver.valid(match[1])) {
+          return match[1]
+        }
+      })
+      .filter((_) => _)
+      .sort((a, b) => {
+        if (semver.lte(b, a)) {
+          if (a === b) return 0
+          return -1
+        }
+      })
   }
 
   async checkNotCommitted() {
