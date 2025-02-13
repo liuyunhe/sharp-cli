ae33d0f635dd70bb5bf41894ce2e6e38a419ea93
liuyunhe <liuyunhe666@aliyun.com>
2025-02-13 17:17:03 +0800

    fix(commit): 优化代码提交流程
    - 添加检查未提交代码的功能
    - 实现远程 master 分支存在性检查和同步
    - 优化 git pull 和 git push 方法
    - 增加错误处理和日志输出

--- imooc-cli/packages/commit/lib/index.js
+++ imooc-cli/packages/commit/lib/index.js
@@ -8,7 +8,9 @@ import {
   initGitServer,
   initGitType,
   clearCache,
-  createRemoteRepo
+  createRemoteRepo,
+  makeInput,
+  makeList
 } from '@sharpcli/utils'
 
 const CACHE_DIR = '.cli-imooc'
@@ -34,7 +36,6 @@ class CommitCommand extends Command {
       clearCache()
     }
 
-    ;('')
     await this.createRemoteRepo()
     await this.initLocal()
   }
@@ -106,16 +107,68 @@ class CommitCommand extends Command {
       this.git.addRemote('origin', remoteUrl)
       log.success('添加git remote', remoteUrl)
     }
-    
+
+    // 检查未提交的代码
+    await this.checkNotCommitted()
+
+    // 检查远程master分支是否存在
+    const tags = await this.git.listRemote(['--refs'])
+    log.verbose('listRemote', tags)
+    console.log(tags)
+    if (tags.indexOf('refs/heads/master') > -1) {
+      // 拉取远程master分支，实现代码同步
+      await this.pullRemoteRepo('master', {
+        // 防止.git被删除之后，无法提交，这里允许不相关的历史记录
+        '--allow-unrelated-histories': null
+      })
+    } else {
+      // 直接推送当前到master分支
+      await this.pushRemoteRepo('master')
+    }
+  }
+
+  async checkNotCommitted() {
     const status = await this.git.status()
-    log.verbose('git status', status)
+    if (
+      status.not_added.length > 0 ||
+      status.created.length > 0 ||
+      status.deleted.length > 0 ||
+      status.modified.length > 0 ||
+      status.renamed.length > 0
+    ) {
+      log.verbose('status', status)
+      await this.git.add(status.not_added)
+      await this.git.add(status.created)
+      await this.git.add(status.deleted)
+      await this.git.add(status.modified)
+      await this.git.add(status.renamed)
+      let message
+      while (!message) {
+        message = await makeInput({
+          message: '请输入提交信息'
+        })
+      }
+      await this.git.commit(message)
+      log.success('本地 commit 提交成功')
+    }
+  }
+
+  async pullRemoteRepo(branch = 'master', options = {}) {
     // 拉取远程master分支，实现代码同步
-    await this.git.pull('origin', 'master').catch((err) => {
-      log.verbose('git pull origin master', err.message)
-      if (err.message.indexOf("Couldn't find remote ref master") >= 0) {
+    await this.git.pull('origin', branch, options).catch((err) => {
+      log.error(`git pull origin ${branch}`, err.message)
+      if (err.message.indexOf('find remote ref master') >= 0) {
         log.warn('获取远程[master]分支失败')
       }
+      // 正常的方式退出
+      process.exit(0)
     })
+  }
+
+  async pushRemoteRepo(branchName) {
+    log.info(`推送代码至远程分支：${branchName}`)
+    await this.git.push('origin', branchName)
+    log.success('推送成功')
   }
 }
 
