9662fa921775860c08e54ab968c9c71e519a2184
liuyunhe <liuyunhe666@aliyun.com>
2025-03-14 15:16:32 +0800

    refactor(test.mjs): 重构代码并添加新的功能
    - 移除了多余的空行和分号，统一代码风格
    - 添加了使用 Promise 化的 question 方法处理用户输入的逻辑
    - 优化了 readline 接口的事件处理
    - 简化了 http 服务器的创建逻辑

--- test.mjs
+++ test.mjs
@@ -1,21 +1,21 @@
 import { fileURLToPath } from 'url'
-import path from 'path';
-import fs from 'fs';
-import http from 'http';
-import readline from 'node:readline';
+import path from 'path'
+import fs from 'fs'
+import http from 'http'
+import readline from 'node:readline'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const rurl = path.resolve('../a', '../url.js');
-const jurl = path.join('../b', '../../url.js');
+const rurl = path.resolve('../a', '../url.js')
+const jurl = path.join('../b', '../../url.js')
 
 console.log('rurl', rurl)
 console.log('jurl', jurl)
 
-const rs = fs.createReadStream('./imooc-cli/package-lock.json');
+const rs = fs.createReadStream('./imooc-cli/package-lock.json')
 
-const ws = fs.createWriteStream('./text.txt');
+const ws = fs.createWriteStream('./text.txt')
 
 let length = 0
 let lineLength = 0
@@ -44,13 +44,50 @@ const nrl = readline.createInterface({
 rl.on('line', (line) => {
   lineLength++
 })
-rl.on('close', () => {
-  console.log('line', lineLength)
-  nrl.question('你的名字是？', (answer) => {
-    console.log('你输入的名字是', answer)
-    nrl.close()
+
+// rl.on('close', () => {
+//   console.log('line', lineLength)
+//   nrl.question('你的名字是？', (answer) => {
+//     console.log('你输入的名字是', answer)
+//     nrl.question('你的年龄是？', (answer) => {
+//       console.log('你输入的年龄是', answer)
+//       nrl.close()
+//     })
+//   })
+// })
+
+// 将 question 方法 Promise 化
+const question = (rlInterface, prompt) => {
+  return new Promise((resolve) => {
+    rlInterface.question(prompt, resolve)
+  }).then((answer) => {
+    return answer
   })
+}
+
+rl.on('close', async () => {
+  console.log('line', lineLength)
+
+  try {
+    const name = await question(nrl, '你的名字是？')
+    console.log('你输入的名字是', name)
+
+    const age = await question(nrl, '你的年龄是？')
+    console.log('你输入的年龄是', age)
+
+    nrl.on('close', () => {
+      console.log({
+        name,
+        age
+      })
+    })
+  } catch (err) {
+    console.error('输入过程中发生错误:', err)
+  } finally {
+    nrl.close()
+  }
 })
+
 const server = http.createServer((req, res) => {
   res.end('hello world123')
 })
@@ -58,8 +95,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log('server is running http://localhost:3000')
 })
-
-
-
-
-
