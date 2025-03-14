b0d069e730c0766dbb3944bfd9ac9b409e4b1ea9
liuyunhe <liuyunhe666@aliyun.com>
2025-03-14 09:33:22 +0800

    feat(test): 添加 HTTP 服务器并优化读取流代码
    - 引入 http 模块并创建简单 HTTP 服务器
    - 调整 readline 接口创建时机，提高代码可读性
    - 修改用户输入提示语，更加友好

--- test.mjs
+++ test.mjs
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url'
 import path from 'path';
 import fs from 'fs';
+import http from 'http';
 import readline from 'node:readline';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,20 +13,9 @@ const jurl = path.join('../b', '../../url.js');
 console.log('rurl', rurl)
 console.log('jurl', jurl)
 
-
-
 const rs = fs.createReadStream('./imooc-cli/package-lock.json');
 
-const rl = readline.createInterface({
-  input: fs.createReadStream('./imooc-cli/package-lock.json')
-})
-
 const ws = fs.createWriteStream('./text.txt');
-
-const nrl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
 
 let length = 0
 let lineLength = 0
@@ -42,15 +32,31 @@ rs.on('end', () => {
 
 rs.pipe(ws)
 
+const rl = readline.createInterface({
+  input: fs.createReadStream('./imooc-cli/package-lock.json')
+})
+
+const nrl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 rl.on('line', (line) => {
   lineLength++
 })
 rl.on('close', () => {
   console.log('line', lineLength)
-  nrl.question('请输入你的名字 ', (answer) => {
+  nrl.question('你的名字是？', (answer) => {
     console.log('你输入的名字是', answer)
     nrl.close()
   })
+})
+const server = http.createServer((req, res) => {
+  res.end('hello world123')
+})
+
+server.listen(3000, () => {
+  console.log('server is running http://localhost:3000')
 })
 
 
